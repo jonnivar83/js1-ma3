f8d48178ae40db8bd1ed8ecb4a7dc4dfb0dcc3b5
jonnivar83 <58147335+jonnivar83@users.noreply.github.com>
2020-04-19 23:53:24 +0200

    Add files via upload

---
+++ jonn-egderod-js1.ma3.js
@@ -0,0 +1,25 @@
+// Question 1
+const subtract = (a, b) => a - b;
+// Question 2
+const baseUrl = "https://api.rawg.io/api/games?genres=sports";
+fetch(baseUrl)
+    .then(function(response) {
+        console.dir(json);
+    })
+    .catch(function(error) {
+        console.dir(error);
+    });
+    // Question 3
+    var str = "These cats are outrageous";
+var res = str.replace(/cats/g, "giraffes");
+// Question 5
+<div class="container">
+    <p>Lorem ipsum dolor</p>
+    <button class="btn">Click me</button>
+</div>
+function createDetails(details) {
+    console.dir(details);
+
+    const container = document.querySelector(".container");
+    const loader = document.querySelector(".btn");
+    container.removeChild(btn);}
